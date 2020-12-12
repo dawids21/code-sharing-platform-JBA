1e658968cad12572f002b6e4274f9df336b0d913
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-12 10:38:18 +0100

    add time in program service

--- src/main/java/platform/service/ProgramService.java
+++ src/main/java/platform/service/ProgramService.java
@@ -2,13 +2,16 @@ package platform.service;
 
 import platform.model.ProgramDto;
 
+import java.time.LocalDateTime;
+
 public class ProgramService {
 
     private ProgramDto programDto;
 
     public ProgramService() {
         programDto = new ProgramDto(
-                 "public class Main {\n public static void main(String[] args) {\n  System.out.println(\"Hello World\");\n }\n}");
+                 "public class Main {\n public static void main(String[] args) {\n  System.out.println(\"Hello World\");\n }\n}",
+                 LocalDateTime.now());
     }
 
     public ProgramDto getProgram() {
