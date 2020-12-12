7f616fdc7f1fb0e697e231ab3d44edcb7c371ff4
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-12 10:06:36 +0100

    create field to hold program dto in program service

--- src/main/java/platform/service/ProgramService.java
+++ src/main/java/platform/service/ProgramService.java
@@ -4,11 +4,14 @@ import platform.model.ProgramDto;
 
 public class ProgramService {
 
+    private ProgramDto programDto;
+
     public ProgramService() {
+        programDto = new ProgramDto(
+                 "public class Main {\n public static void main(String[] args) {\n  System.out.println(\"Hello World\");\n }\n}");
     }
 
     public ProgramDto getProgram() {
-        return new ProgramDto(
-                 "public class Main {\n public static void main(String[] args) {\n  System.out.println(\"Hello World\");\n }\n}");
+        return programDto;
     }
 }
