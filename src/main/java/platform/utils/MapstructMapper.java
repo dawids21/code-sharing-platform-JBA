5ee313cfa80adb3f8b96c3f623633bf58dd0001e
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-12 10:39:02 +0100

    add mapping from created to date in dto

--- src/main/java/platform/utils/MapstructMapper.java
+++ src/main/java/platform/utils/MapstructMapper.java
@@ -1,11 +1,13 @@
 package platform.utils;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import platform.model.Program;
 import platform.model.ProgramDto;
 
 @Mapper
 public interface MapstructMapper {
 
+    @Mapping(source = "created", target = "date")
     ProgramDto programToProgramDto(Program program);
 }
