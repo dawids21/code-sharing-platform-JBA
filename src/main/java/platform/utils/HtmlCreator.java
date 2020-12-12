dace489f88454ae736a37e4ae36da3874cd689fe
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-12 12:09:12 +0100

    implement html create for /code page

--- src/main/java/platform/utils/HtmlCreator.java
+++ src/main/java/platform/utils/HtmlCreator.java
@@ -4,8 +4,14 @@ import platform.model.ProgramDto;
 
 public class HtmlCreator {
 
-    public String programPage(ProgramDto programDto) {
+    public static final String SEND_FUNCTION = "function send() { " +
+                                               "let object = { \"code\": document.getElementById(\"code_snippet\").value }; let json = JSON.stringify(object); let xhr = new XMLHttpRequest(); " +
+                                               "xhr.open(\"POST\", '/api/code/new', false); xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');" +
+                                               "xhr.send(json); if (xhr.status == 200) { alert(\"Success!\"); } }";
 
-        throw new UnsupportedOperationException("Not implemented yet");
+    public String programPage(ProgramDto programDto) {
+        return "<html><head><title>Code</title></head><body><span id=\"load_date\">" +
+               programDto.getDate() + "</span><pre id=\"code_snippet\">" +
+               programDto.getCode() + "</pre></body></html> " + SEND_FUNCTION;
     }
 }
