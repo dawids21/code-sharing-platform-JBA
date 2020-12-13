e02873ca86bb04f034e276fdbd622e1d941da9d4
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-13 14:43:20 +0100

    add style to html creator

--- src/main/java/platform/utils/HtmlCreator.java
+++ src/main/java/platform/utils/HtmlCreator.java
@@ -9,9 +9,14 @@ public class HtmlCreator {
                                                "xhr.open(\"POST\", '/api/code/new', false); xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');" +
                                                "xhr.send(json); if (xhr.status == 200) { alert(\"Success!\"); } }";
 
+    public static final String STYLE =
+             "<style>#load_date { color: green; } #code_snippet { background-color: lightgrey; border: 2px solid black; }</style>";
+
     public String programPage(ProgramDto programDto) {
-        return "<html><head><title>Code</title></head><body><span id=\"load_date\">" +
+        return "<html><head>" + STYLE +
+               "<title>Code</title></head><body><span id=\"load_date\">" +
                programDto.getDate() + "</span><pre id=\"code_snippet\">" +
-               programDto.getCode() + "</pre></body></html> " + SEND_FUNCTION;
+               programDto.getCode() + "</pre></body><script>" + SEND_FUNCTION +
+               "</script></html>";
     }
 }
