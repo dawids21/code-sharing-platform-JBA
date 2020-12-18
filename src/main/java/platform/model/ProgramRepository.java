747bdcc6c0482e15c40f34573119645801337825
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-18 22:39:06 +0100

    add method in ProgramRepository to sort by created with limit

--- src/main/java/platform/model/ProgramRepository.java
+++ src/main/java/platform/model/ProgramRepository.java
@@ -1,12 +1,12 @@
 package platform.model;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ProgramRepository extends JpaRepository<Program, Long> {
 
-    List<Program> findAllOrderByCreatedDesc();
+    Page<Program> findAllByOrderByCreatedDesc(Pageable pageable);
 }
