4692f9bc2a82d0b2416ade8bd8f42a4713bc903e
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-18 19:45:44 +0100

    add method for sorting by created date in repository

--- src/main/java/platform/model/ProgramRepository.java
+++ src/main/java/platform/model/ProgramRepository.java
@@ -3,7 +3,10 @@ package platform.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProgramRepository extends JpaRepository<Program, Long> {
 
+    List<Program> findAllOrderByCreatedDesc();
 }
