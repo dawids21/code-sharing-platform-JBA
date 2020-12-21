1e2f89b328dcecd391617ef67b801b78ee85cce0
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-21 22:59:03 +0100

    create class for getting date after given seconds

---
+++ src/main/java/platform/model/ProgramExpireTimeCalculator.java
@@ -0,0 +1,25 @@
+package platform.model;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
+
+public class ProgramExpireTimeCalculator {
+
+    private final Clock clock;
+
+    public ProgramExpireTimeCalculator(Clock clock) {
+        this.clock = clock;
+    }
+
+    public int secondsRemain(LocalDateTime date) {
+        LocalDateTime now = LocalDateTime.now(clock);
+        return (int) SECONDS.between(now, date);
+    }
+
+    public LocalDateTime dateAfterSeconds(int toAdd) {
+        return LocalDateTime.now(clock)
+                            .plusSeconds(toAdd);
+    }
+}
