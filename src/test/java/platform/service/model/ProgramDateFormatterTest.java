2553bdec5e8ccbf42eaec6c248deee7f370fc401
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-23 18:37:37 +0100

    add test for throwing exception in ProgramDateFormatter

--- src/test/java/platform/service/model/ProgramDateFormatterTest.java
+++ src/test/java/platform/service/model/ProgramDateFormatterTest.java
@@ -8,9 +8,11 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ProgramDateFormatterTest {
@@ -37,6 +39,13 @@ class ProgramDateFormatterTest {
         assertThat(date).isEqualTo(expected);
     }
 
+    @ParameterizedTest
+    @MethodSource("provideNonParsableDates")
+    void should_throw_exception_when_non_parsable_date(String nonParsableDate) {
+        assertThatThrownBy(() -> programDateFormatter.toLocalDateTime(
+                 nonParsableDate)).isInstanceOf(DateTimeParseException.class);
+    }
+
     private static Stream<Arguments> provideDatesForFormatTests() {
         return Stream.of(Arguments.of(LocalDateTime.of(2020, 12, 12, 12, 12, 12)),
                          Arguments.of(LocalDateTime.of(22, 2, 1, 1, 1, 1)),
@@ -53,5 +62,12 @@ class ProgramDateFormatterTest {
                                       LocalDateTime.of(2, 1, 1, 1, 1, 1)),
                          Arguments.of("0220-01-01 01:01:01",
                                       LocalDateTime.of(220, 1, 1, 1, 1, 1)));
+    }
+
+    private static Stream<Arguments> provideNonParsableDates() {
+        return Stream.of(Arguments.of("2020-2-12 12:12:12"),
+                         Arguments.of("22-01-01 01:01:01"),
+                         Arguments.of("0002-01-01 01:01:1"),
+                         Arguments.of("0220-01-01 1:01:01"));
     }
 }
