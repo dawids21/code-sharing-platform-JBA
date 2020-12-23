824615a0ce316ef9fc724bc1f3178ca0d4ca92af
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-23 17:12:59 +0100

    remove tests for program service

---
+++ src/test/java/platform/integration/DatabaseTest.java
@@ -0,0 +1,29 @@
+package platform.integration;
+
+class DatabaseTest {
+
+    //TODO test what happened if i request more programs than there is in database
+
+    //TODO test sorting of programs by date
+
+    //    @Test
+    //    void should_return_requested_num_of_programs_when_there_is_enough_in_memory() {
+    //        addNPrograms(11);
+    //        List<ProgramDto> programs = programService.getLastPrograms(10);
+    //        assertThat(programs).hasSize(10);
+    //    }
+    //
+    //    @Test
+    //    void should_return_max_num_of_programs_when_there_is_less_in_memory() {
+    //        int numOfRequested = 6;
+    //        addNPrograms(numOfRequested);
+    //        List<ProgramDto> programs = programService.getLastPrograms(10);
+    //        assertThat(programs).hasSize(numOfRequested);
+    //    }
+    //
+    //    @Test
+    //    void should_return_empty_list_when_no_programs_in_memory() {
+    //        List<ProgramDto> programs = programService.getLastPrograms(10);
+    //        assertThat(programs).hasSize(0);
+    //    }
+}
