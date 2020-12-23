66d9ac5760f5928441b4d9175be2d02574046414
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-23 08:40:41 +0100

    add tests for scheduled task

---
+++ src/main/java/platform/service/ScheduledDatabaseRemoveRecords.java
@@ -0,0 +1,22 @@
+package platform.service;
+
+import platform.model.ProgramRepository;
+
+import java.time.Clock;
+
+public class ScheduledDatabaseRemoveRecords {
+
+    private final ProgramRepository programRepository;
+    private final Clock clock;
+
+    public ScheduledDatabaseRemoveRecords(ProgramRepository programRepository,
+                                          Clock clock) {
+        //TODO implement ScheduledDatabaseRemoveRecords
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void removeRecords() {
+        //TODO implement removeRecords
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
