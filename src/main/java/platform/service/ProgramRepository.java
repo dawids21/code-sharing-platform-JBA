44d6666e5cde234a71d5f70fa45d9c6a0af142c4
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-28 11:05:15 +0100

    change type of id to uuid in repository

--- src/main/java/platform/service/ProgramRepository.java
+++ src/main/java/platform/service/ProgramRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import platform.service.model.Program;
 
+import java.util.UUID;
+
 @Repository
-public interface ProgramRepository extends JpaRepository<Program, Long> {
+public interface ProgramRepository extends JpaRepository<Program, UUID> {
 
     @Query("SELECT p FROM Program p WHERE p.restricted = false ORDER BY p.created DESC")
     Page<Program> findNotRestricted(Pageable pageable);
