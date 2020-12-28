5cbc9cafc1f8bf71e4956551c9221e42ae2a5cf1
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-28 19:20:16 +0100

    implement tests for ProgramViewsReducer

--- src/main/java/platform/service/ProgramViewsReducer.java
+++ src/main/java/platform/service/ProgramViewsReducer.java
@@ -1,5 +1,13 @@
 package platform.service;
 
+import platform.service.model.Program;
+
+import java.util.Optional;
+
 public class ProgramViewsReducer {
 
+    public Optional<Program> reduce(Optional<Program> program) {
+        //TODO implement reduce
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
