e0dddd500c868dad7bc0c0c04462c5ec77dbf9eb
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-28 18:51:22 +0100

    add views restriction to new program form

--- src/resources/static/js/script.js
+++ src/resources/static/js/script.js
@@ -2,6 +2,7 @@ function send() {
     let object = {
         "code": document.getElementById("code_snippet").value,
         "time": document.getElementById("time_restriction").value
+        "views": document.getElementById("views_restriction").value
     };
     let json = JSON.stringify(object);
     let xhr = new XMLHttpRequest();
