ff9b0dc8b834d11378aa8c942042e6b332b19804
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-29 07:53:29 +0100

    create tests for restriction checker

---
+++ src/main/java/platform/service/RestrictionChecker.java
@@ -0,0 +1,17 @@
+package platform.service;
+
+import platform.service.model.Program;
+
+public class RestrictionChecker {
+
+
+    public RestrictionChecker(CurrentDateGetter currentDateGetter) {
+        //TODO implement RestrictionChecker
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean check(Program program) {
+        //TODO implement check
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
