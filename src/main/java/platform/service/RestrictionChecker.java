88318cb56be99376201cf5ca52dfad876f736940
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-29 08:02:10 +0100

    implement RestrictionChecker

--- src/main/java/platform/service/RestrictionChecker.java
+++ src/main/java/platform/service/RestrictionChecker.java
@@ -2,16 +2,29 @@ package platform.service;
 
 import platform.service.model.Program;
 
+import java.time.LocalDateTime;
+
 public class RestrictionChecker {
 
+    private final CurrentDateGetter currentDateGetter;
 
     public RestrictionChecker(CurrentDateGetter currentDateGetter) {
-        //TODO implement RestrictionChecker
-        throw new UnsupportedOperationException("Not implemented yet");
+        this.currentDateGetter = currentDateGetter;
     }
 
     public boolean check(Program program) {
-        //TODO implement check
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (!program.isRestricted()) {
+            return true;
+        }
+        return checkTimeRestriction(program) && checkViewRestriction(program);
+    }
+
+    private boolean checkViewRestriction(Program program) {
+        return program.getViews() > 0;
+    }
+
+    private boolean checkTimeRestriction(Program program) {
+        LocalDateTime now = currentDateGetter.now();
+        return now.isBefore(program.getValidUntil());
     }
 }
